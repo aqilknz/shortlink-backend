fae03544796740560f62f97b71c344f4c976c2db
aqilknz <khairunnz123@gmail.com>
2026-06-18 07:18:00 +0700

    feat: implement main router with dependency injection and swagger setup

--- internal/router/main.router.go
+++ internal/router/main.router.go
@@ -1,20 +1,36 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/aqilknz/shortlink-backend/internal/controller"
+	"github.com/aqilknz/shortlink-backend/internal/middleware"
+	"github.com/aqilknz/shortlink-backend/internal/repository"
+	"github.com/aqilknz/shortlink-backend/internal/service"
+	"github.com/aqilknz/shortlink-backend/pkg"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
+
+	_ "github.com/aqilknz/shortlink-backend/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func InitRouter(app *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
-	// Endpoint untuk testing (Health Check)
-	app.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-			"status":  "Database & Redis Connected!",
-		})
-	})
+func InitRouter(app *gin.Engine, db *pgxpool.Pool, redis *redis.Client) {
+	app.Use(middleware.CORSMiddleware)
+	// Middleware Global
+	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
+	authRepo := repository.NewAuthRepository(db, redis)
+	linkRepo := repository.NewLinkRepository(db)
+	hashConfig := pkg.NewHashConfig()
+	authService := service.NewAuthService(authRepo, hashConfig)
+	linkService := service.NewLinkService(linkRepo)
+
+	linkController := controller.NewLinkController(linkService)
+	authController := controller.NewAuthController(authService)
+
+	api := app.Group("/api")
+	RegisterAuthRoutes(api, authController, authRepo)
+	RegisterLinkRoutes(api, linkController, authRepo)
+	RegisterRedirectRoute(app, linkController)
 }
